b82138a47d1c0c95e1e2b332a4cc3ef594650119
komron2052@gmail.com <komron2052@gmail.com>
2023-02-23 14:05:10 +0500

    xxxx

--- MyCash.Data/Configurations/DataBasePath.cs
+++ MyCash.Data/Configurations/DataBasePath.cs
@@ -5,9 +5,9 @@ namespace MyCash.Data.Configurations;
 
 public class DataBasePath
 {
-    public const string USER_PATH = "D:\\MyCash\\MyCash.Data\\DataBases\\User.json";
-    public const string WALLET_PATH = "D:\\MyCash\\MyCash.Data\\DataBases\\Wallet.json";
-    public const string INCOME_PATH = "D:\\MyCash\\MyCash.Data\\DataBases\\Income.json";
-    public const string EXPOSE_PATH = "D:\\MyCash\\MyCash.Data\\DataBases\\Expose.json";
-    public const string CATEGORY_PATH = "D:\\MyCash\\MyCash.Data\\DataBases\\Category.json";
+    public const string USER_PATH = "..\\..\\..\\..\\..\\MyCash\\MyCash.Data\\DataBases\\User.json";
+    public const string WALLET_PATH = "..\\..\\..\\..\\..\\MyCash\\MyCash.Data\\DataBases\\Wallet.json";
+    public const string INCOME_PATH = "..\\..\\..\\..\\..\\MyCash\\MyCash.Data\\DataBases\\Income.json";
+    public const string EXPOSE_PATH = "..\\..\\..\\..\\..\\MyCash\\MyCash.Data\\DataBases\\Expose.json";
+    public const string CATEGORY_PATH = "..\\..\\..\\..\\..\\MyCash\\MyCash.Data\\DataBases\\Category.json";
 }
